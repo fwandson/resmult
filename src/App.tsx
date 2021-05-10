0f0767f1ed0e16a70ca8cffcfbab2ac1513af19b
ericsonmoreira <moreira.ericson@gmail.com>
2021-05-10 14:36:53 -0300

    add lib react-helmet

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
+import { Typography } from '@material-ui/core';
+import { ThemeProvider } from '@material-ui/styles';
+import 'fontsource-roboto';
+import theme from 'src/theme';
+import GlobalStyles from './components/GlobalStyles';
+
 function App() {
-  return <div>Ola mundo</div>;
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <Typography>APP</Typography>
+      <Typography variant="caption">Testando</Typography>
+    </ThemeProvider>
+  );
 }
 
 export default App;
