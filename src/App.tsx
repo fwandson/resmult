c1a671b676f108e82cc36a59fb09aa90c9af2a11
ericsonmoreira <moreira.ericson@gmail.com>
2021-05-10 17:36:04 -0300

    adjusts

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,15 @@
-import { Typography } from "@material-ui/core";
-import { ThemeProvider } from "@material-ui/styles";
-import "fontsource-roboto";
-import { useRoutes } from "react-router";
-import theme from "src/theme";
-import GlobalStyles from "./components/GlobalStyles";
-import routes from "./routes";
+import { Typography } from '@material-ui/core';
+import { ThemeProvider } from '@material-ui/styles';
+import 'fontsource-roboto';
+import theme from 'src/theme';
+import GlobalStyles from './components/GlobalStyles';
 
 function App() {
-  const routing = useRoutes(routes);
-
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      {routing}
+      <Typography>Ola mundo</Typography>
+      <Typography variant="h1">Teste</Typography>
     </ThemeProvider>
   );
 }
