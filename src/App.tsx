c735f2fdefdfbfdb3dee0a373b57dfe2506e181d
ericsonmoreira <moreira.ericson@gmail.com>
2021-05-10 15:10:02 -0300

    add more pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
-import { Typography } from '@material-ui/core';
-import { ThemeProvider } from '@material-ui/styles';
-import 'fontsource-roboto';
-import theme from 'src/theme';
-import GlobalStyles from './components/GlobalStyles';
+import { Typography } from "@material-ui/core";
+import { ThemeProvider } from "@material-ui/styles";
+import "fontsource-roboto";
+import { useRoutes } from "react-router";
+import theme from "src/theme";
+import GlobalStyles from "./components/GlobalStyles";
+import routes from "./routes";
 
 function App() {
+  const routing = useRoutes(routes);
+
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Typography>APP</Typography>
-      <Typography variant="caption">Testando</Typography>
+      {routing}
     </ThemeProvider>
   );
 }
