561f30fb7ffade8974c916a678d75faca3559df1
ericsonmoreira <moreira.ericson@gmail.com>
2021-05-12 11:46:08 -0300

    add react-toastify lib and configure #2

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 import { ThemeProvider } from '@material-ui/styles';
 import 'fontsource-roboto';
+import { BrowserRouter } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.min.css';
+import Routes from 'src/routes';
 import theme from 'src/theme';
 import GlobalStyles from './components/GlobalStyles';
-import Routes from 'src/routes';
-import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyles />
+      <ToastContainer />
       <BrowserRouter>
         <AuthProvider>
           <Routes />
