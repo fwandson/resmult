e6f5ed29069b7aef0c28b6956eb7e31648f6890c
ericsonmoreira <moreira.ericson@gmail.com>
2021-05-12 12:07:17 -0300

    add Toast config #2

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
 import { ThemeProvider } from '@material-ui/styles';
 import 'fontsource-roboto';
 import { BrowserRouter } from 'react-router-dom';
-import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.min.css';
 import Routes from 'src/routes';
 import theme from 'src/theme';
 import GlobalStyles from './components/GlobalStyles';
+import CustonToastContainer from './config/CustonToastContainer';
 import { AuthProvider } from './context/AuthContext';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <ToastContainer />
+      <CustonToastContainer />
       <BrowserRouter>
         <AuthProvider>
           <Routes />
