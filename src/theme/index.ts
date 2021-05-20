f17785be7a23a356ebd500c64500064ae6e215d6
ericsonmoreira <moreira.ericson@gmail.com>
2021-05-20 16:13:37 -0300

    adjusts in theme palette #1

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -10,7 +10,21 @@ const theme = createMuiTheme({
     },
     primary: {
       contrastText: '#ffffff',
+      light: '#D1DEFF',
       main: '#5EA5E4',
+      dark: '#092053',
+    },
+    secondary: {
+      main: '#FF5348',
+    },
+    error: {
+      main: '#AB122E',
+    },
+    success: {
+      main: '#F49869',
+    },
+    warning: {
+      main: '#F5AF58',
     },
     text: {
       primary: '#172b4d',
