d9e48cb43482d35d691c9740952b7892f5608d39
ericsonmoreira <moreira.ericson@gmail.com>
2021-05-22 12:44:46 -0300

    add GenericContent #3

---
+++ src/components/GenericContent/index.tsx
@@ -0,0 +1,34 @@
+import { Box, Typography } from '@material-ui/core';
+import { Helmet } from 'react-helmet';
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+`;
+
+interface GenericContentProps {
+  helmetText: string;
+  title: string;
+}
+
+const GenericContent: React.FC<GenericContentProps> = ({
+  children,
+  helmetText,
+  title,
+}) => {
+  return (
+    <Container>
+      <Helmet>
+        <title>{helmetText}</title>
+      </Helmet>
+      <Box marginBottom={4}>
+        <Typography variant="h1">Minhas turmas</Typography>
+      </Box>
+      {children}
+    </Container>
+  );
+};
+
+export default GenericContent;
