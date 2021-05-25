44cd96f25990a253eb474d99e46617affc5563f9
ericsonmoreira <moreira.ericson@gmail.com>
2021-05-25 09:21:27 -0300

    add turmas.getOfertas and types #3

--- src/resources/turmas/index.ts
+++ src/resources/turmas/index.ts
@@ -1,12 +1,14 @@
 import { AxiosResponse } from 'axios';
 import api from 'src/api';
-import { GetTurmasReturn } from './types';
+import { GetOfertasTurmasReturn, GetTurmasReturn } from './types';
 
 const base = '/residencia-multiprofissional/supervisores/turmas';
 
 const turmas = {
   base,
   get: (): Promise<AxiosResponse<GetTurmasReturn>> => api.get(base),
+  getOfertas: (id: number): Promise<AxiosResponse<GetOfertasTurmasReturn>> =>
+    api.get(`/residencia-multiprofissional/supervisores/turma/${id}/ofertas`),
 };
 
 export default turmas;
--- src/resources/turmas/types.ts
+++ src/resources/turmas/types.ts
@@ -1,3 +1,4 @@
+// Get Turmas
 export interface Turma {
   id: number;
   codigoTurma: string;
@@ -10,4 +11,34 @@ export interface Turma {
 
 export interface GetTurmasReturn {
   turmas: Turma[];
+}
+
+// Get Ofertas Turma
+export interface Modulo {
+  nome: string;
+  id: number;
+}
+
+export interface OfertasModulo {
+  id: number;
+  dataInicio: string;
+  dataFim: string;
+  encerramento?: unknown;
+  nome: string;
+  semestre: number;
+  semestre_descricao: string;
+  turma: {
+    id?: number;
+    codigoTurma: string;
+    descricao: string;
+    dataInicio?: Date;
+    dataFim?: Date;
+  };
+  modulo: Modulo;
+  cargahoraria: string;
+  unidadetematicaid: number;
+}
+
+export interface GetOfertasTurmasReturn {
+  ofertasModulos: OfertasModulo[];
 }
