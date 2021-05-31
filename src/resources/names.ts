fb6dadaad33e32791b095c8aa7f2bfef8242cf79
ericsonmoreira <moreira.ericson@gmail.com>
2021-05-31 11:31:43 -0300

    add tiposCargaHoraria resource #3

--- src/resources/names.ts
+++ src/resources/names.ts
@@ -2,6 +2,7 @@ enum RESOURCE_URLS {
   LOGIN = '/auth',
   GET_TURMAS = '/residencia-multiprofissional/supervisores/turmas',
   GET_TURMAS_OFERTAS = '/residencia-multiprofissional/supervisores/turma/:id/ofertas',
+  GET_TIPOS_CARGA_HORARIA = '/residencia-multiprofissional/carga-horaria/tipos',
 }
 
 export default RESOURCE_URLS;
---
+++ src/resources/tiposCargaHoraria/types.ts
@@ -0,0 +1,6 @@
+export declare namespace GetNames {
+  export interface Return {
+    id: string;
+    descricao: string;
+  }
+}
