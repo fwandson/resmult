b92c2bb1c8fe8f489470fd1fc560fc896f1bcbe5
ericsonmoreira <moreira.ericson@gmail.com>
2021-06-06 20:05:32 -0300

    adjusts in env files #4

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,17 +1,9 @@
 import axios from 'axios';
 
-// TODO: pesquisar mais como usar os .envs para tipos de hambiente (test, dev, production)
-export const getBaseURL = () => {
-  const url = {
-    development: process.env.REACT_APP_API_BASE_URL_DEV,
-    production: process.env.REACT_APP_API_BASE_URL_PROD,
-    test: process.env.REACT_APP_API_BASE_URL_TEST,
-  };
-  return url[process.env.NODE_ENV] || process.env.REACT_APP_API_BASE_URL_PROD;
-};
-
 const api = axios.create({
-  baseURL: getBaseURL(),
+  baseURL: process.env.REACT_APP_API_BASE_URL,
 });
+
+// TODO: pesquisar depois -> como usar interceptions para testes
 
 export default api;
