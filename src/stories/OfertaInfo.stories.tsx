71def6234c2dff286b3a7d6b92ea154f677f67dd
ericsonmoreira <moreira.ericson@gmail.com>
2021-06-08 09:35:18 -0300

    add argTypes in OfertaInfo stotybook

--- src/stories/OfertaInfo.stories.tsx
+++ src/stories/OfertaInfo.stories.tsx
@@ -4,6 +4,32 @@ import OfertaInfo, { OfertaInfoProps } from 'src/components/OfertaInfo';
 export default {
   title: 'Components/OfertaInfo',
   component: OfertaInfo,
+  argTypes: {
+    nome: {
+      description: 'Nome da Oferta',
+    },
+    periodo: {
+      description: 'Período da Oferta',
+    },
+    cargaHoraria: {
+      description: 'Carga Horária da Oferta',
+    },
+    cod: {
+      description: 'Código da Oferta',
+    },
+    inicio: {
+      description: 'Data inicial',
+      control: {
+        type: 'date',
+      },
+    },
+    fim: {
+      description: 'Data final',
+      control: {
+        type: 'date',
+      },
+    },
+  },
 } as Meta;
 
 const Template: Story<OfertaInfoProps> = (args) => <OfertaInfo {...args} />;
@@ -12,8 +38,8 @@ export const Default = Template.bind({});
 Default.args = {
   cod: 'T2HOSPITALAR',
   nome: 'Turma II',
-  inicio: '20/20/2020',
-  fim: '21/21/2021',
+  inicio: new Date(),
+  fim: new Date(),
   periodo: 'Primeiro ano',
-  cargaHoraria: '1000h',
+  cargaHoraria: '1000',
 };
