0039211e722fa2990b67ee94cfe3a8f9604afc5d
ericsonmoreira <moreira.ericson@gmail.com>
2021-06-08 14:57:39 -0300

    adjusts in testes #6

--- src/__test__/components/SimpleTable.test.tsx
+++ src/__test__/components/SimpleTable.test.tsx
@@ -3,8 +3,6 @@ import SimpleTable from 'src/components/SimpleTable';
 
 const title = 'Alunos';
 
-const headCells = ['Nome', 'Idade', 'Sexo'];
-
 const rows = [
   ['Ericson', 33, 'M'],
   ['Marilia', 28, 'F'],
@@ -13,14 +11,19 @@ const rows = [
 describe('components/SimpleTable', () => {
   test('create', async () => {
     const { getByText } = render(
-      <SimpleTable title={title} headCells={headCells} rows={rows} />
+      <SimpleTable
+        title={title}
+        headCells={[
+          { value: 'Nome', align: 'left' },
+          { value: 'Idade', align: 'left' },
+          { value: 'Sexo', align: 'right' },
+        ]}
+        rows={rows}
+      />
     );
 
     // verificando title
     expect(getByText(title));
-
-    // verificando head cells
-    headCells.forEach((cell) => expect(getByText(cell)));
 
     // verificando rows cells
     rows.forEach((row) => row.forEach((cell) => expect(getByText(cell))));
