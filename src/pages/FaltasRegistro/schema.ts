b60fe425ddb25b74b513fdd2b8b2fd321ad2aac3
ericsonmoreira <moreira.ericson@gmail.com>
2021-06-10 16:15:31 -0300

    add schema validation (adjusts) #6

--- src/pages/FaltasRegistro/schema.ts
+++ src/pages/FaltasRegistro/schema.ts
@@ -3,15 +3,21 @@ import yup from 'src/config/yup';
 const schema = yup.object().shape({
   ch: yup.array().of(
     yup.object().shape({
-      pratica: yup.number().min(0, 'Nota inválida').max(10, 'Nota invalida'),
+      pratica: yup
+        .number()
+        .min(0, 'Nota inválida')
+        .max(10, 'Nota invalida')
+        .typeError('Nota inválida'),
       teoricoConceitual: yup
         .number()
         .min(0, 'Nota inválida')
-        .max(10, 'Nota invalida'),
+        .max(10, 'Nota invalida')
+        .typeError('Nota inválida'),
       teoricoPratica: yup
         .number()
         .min(0, 'Nota inválida')
-        .max(10, 'Nota invalida'),
+        .max(10, 'Nota invalida')
+        .typeError('Nota inválida'),
     })
   ),
 });
