57cff046e01e776592b56510131de93319eda388
ericsonmoreira <moreira.ericson@gmail.com>
2021-06-11 08:24:04 -0300

    lint in .storybook files

--- .storybook/manager.js
+++ .storybook/manager.js
@@ -1,5 +1,5 @@
-import { addons } from "@storybook/addons";
-import sbTheme from "./sbTheme";
+import { addons } from '@storybook/addons';
+import sbTheme from './sbTheme';
 
 addons.setConfig({
   theme: sbTheme,
