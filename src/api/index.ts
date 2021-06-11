e7ad3bd1d8a9dfe3418060b3e3d3ae981e0bc725
ericsonmoreira <moreira.ericson@gmail.com>
2021-06-11 08:16:11 -0300

    add interceptor in api #6

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,4 +1,9 @@
-import axios from 'axios';
+import axios, { AxiosResponse } from 'axios';
+import CONSTANTS from 'src/config';
+import NAMES from 'src/routes/names';
+import { cache } from 'swr';
+
+const { LH_TOKEN_NAME } = CONSTANTS;
 
 const delay = (amount: number) =>
   new Promise((resolve) => setTimeout(resolve, amount));
@@ -12,6 +17,21 @@ api.interceptors.response.use(async (request) => {
     await delay(Number(process.env.REACT_APP_API_DELAY));
   }
   return request;
+});
+
+// Interceptors para o caso de usuário não autorizado
+api.interceptors.response.use(undefined, (err: AxiosResponse) => {
+  if (err.status === 401) {
+    // --> limpar o cache (limpando tudo)
+    cache.clear();
+    // --> Remove a token do local storage
+    localStorage.removeItem(LH_TOKEN_NAME);
+    // --> Remove authorization do header
+    api.defaults.headers.authorization = undefined;
+    // --> redireciona para o login
+    window.location.replace(NAMES.LOGIN);
+  }
+  return Promise.reject(err);
 });
 
 export default api;
