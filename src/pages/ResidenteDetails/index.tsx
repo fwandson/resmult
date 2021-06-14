ecabbbc5b42d2be6e161e06aa2113855934de623
ericsonmoreira <moreira.ericson@gmail.com>
2021-06-14 14:53:37 -0300

    adjusts in card info #5

--- src/pages/ResidenteDetails/index.tsx
+++ src/pages/ResidenteDetails/index.tsx
@@ -1,22 +1,29 @@
 import {
   Typography,
   Card,
+  CardHeader,
+  CardActions,
   CardContent,
   Avatar,
   Box,
   createStyles,
   Theme,
   makeStyles,
+  IconButton,
 } from '@material-ui/core';
 import { useParams } from 'react-router';
 import GenericContent from 'src/components/GenericContent';
+import usePessoa from 'src/hooks/usePessoa';
 import useResidentes from 'src/hooks/useResidentes';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import ShareIcon from '@material-ui/icons/Share';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     large: {
-      width: theme.spacing(8),
-      height: theme.spacing(8),
+      width: theme.spacing(6),
+      height: theme.spacing(6),
     },
   })
 );
@@ -36,22 +43,26 @@ const ResidenteDetails: React.FC = () => {
 
   const classes = useStyles();
 
-  const { data, findResidente } = useResidentes({
+  const { data: residentesDataReturn, findResidente } = useResidentes({
     idOferta,
     idTurma,
   });
 
   const residente = findResidente({ id: Number(idResidente) });
 
+  const { data: pessoa } = usePessoa({
+    idPessoa: Number(residente?.person.id),
+  });
+
   return (
     <GenericContent
       helmetText="Residente | Sagu"
       title="Residente"
-      isLoading={!data}
+      isLoading={!residentesDataReturn || !pessoa}
     >
       <Card>
-        <CardContent>
-          <Box display="flex" alignItems="center" mb={2}>
+        <CardHeader
+          avatar={
             <Avatar
               className={classes.large}
               src={`/static/images/avatars/avatar_${
@@ -60,26 +71,60 @@ const ResidenteDetails: React.FC = () => {
             >
               {idResidente}
             </Avatar>
-            <Box ml={2}>
-              <Typography variant="h2">{residente?.person.name}</Typography>
-              <Typography variant="body2" color="textSecondary">
-                {residente?.nucleoProfissional.descricao}
-              </Typography>
-            </Box>
-          </Box>
+          }
+          title={residente?.person.name}
+          subheader={residente?.nucleoProfissional.descricao}
+        />
+        <CardContent>
           <Box>
+            <Typography variant="body1" gutterBottom>
+              Informações
+            </Typography>
             <Typography variant="body2" color="textSecondary" gutterBottom>
               Instituição executora:{' '}
               {residente?.instituicaoExecutoraPerson.name}
             </Typography>
-            <Typography variant="body2" color="textSecondary">
+            <Typography variant="body2" color="textSecondary" gutterBottom>
               Instituição formadora:{' '}
               {residente?.instituicaoFormadoraPerson.name}
             </Typography>
+            <Typography variant="body2" color="textSecondary" gutterBottom>
+              Email: {pessoa?.email}
+            </Typography>
+
+            <Typography variant="body1" gutterBottom>
+              Endereço
+            </Typography>
+
+            <Typography variant="body2" color="textSecondary" gutterBottom>
+              Logradouro: {pessoa?.logradouro}
+            </Typography>
+            <Typography variant="body2" color="textSecondary" gutterBottom>
+              Número: {pessoa?.numero}
+            </Typography>
+            <Typography variant="body2" color="textSecondary" gutterBottom>
+              Bairro: {pessoa?.bairro}
+            </Typography>
+            <Typography variant="body2" color="textSecondary" gutterBottom>
+              Cidade: {pessoa?.cidade.nome}
+            </Typography>
+            <Typography variant="body2" color="textSecondary" gutterBottom>
+              Estado: {pessoa?.cidade.estado.nome}
+            </Typography>
           </Box>
         </CardContent>
+        <CardActions disableSpacing>
+          <IconButton aria-label="add to favorites">
+            <FavoriteIcon />
+          </IconButton>
+          <IconButton aria-label="share">
+            <ShareIcon />
+          </IconButton>
+          <IconButton>
+            <ExpandMoreIcon />
+          </IconButton>
+        </CardActions>
       </Card>
-      <pre>{JSON.stringify(residente, null, 2)}</pre>
     </GenericContent>
   );
 };
