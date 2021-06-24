3395338851ec3f7fad6faafe88759756378369a0
ericsonmoreira <moreira.ericson@gmail.com>
2021-06-24 09:39:13 -0300

    minor adjustments in ViewCHComplementarModal #6

--- src/components/modals/ViewCHComplementarModal/index.tsx
+++ src/components/modals/ViewCHComplementarModal/index.tsx
@@ -1,4 +1,5 @@
 import {
+  Box,
   Button,
   Dialog,
   DialogActions,
@@ -13,6 +14,7 @@ import CheckIcon from '@material-ui/icons/Check';
 import CloseIcon from '@material-ui/icons/Close';
 import { Dispatch, SetStateAction } from 'react';
 import { GetResidentesNames } from 'src/resources/turmas/types';
+import { reduce } from 'lodash';
 
 export interface ViewCHComplementarModalProps extends DialogProps {
   setOpen: Dispatch<SetStateAction<boolean>>;
@@ -31,6 +33,12 @@ const ViewCHComplementarModal: React.FC<ViewCHComplementarModalProps> = (
 ) => {
   const { open, setOpen, residente, ...rest } = props;
 
+  const totalCHComplementates = reduce(
+    residente?.cargahorariacomplementar,
+    (sum, elem) => sum + Number(elem.cargaHoraria),
+    0
+  );
+
   return (
     <Dialog open={open} {...rest} fullWidth>
       <DialogTitle>
@@ -42,8 +50,29 @@ const ViewCHComplementarModal: React.FC<ViewCHComplementarModalProps> = (
         </Grid>
       </DialogTitle>
       <DialogContent>
+        <Box mb={2}>
+          <Typography gutterBottom>
+            Residente: {residente?.person.name}
+          </Typography>
+          <Typography gutterBottom>
+            Carga horária pendente: {residente?.cargahorariapendente} horas
+          </Typography>
+        </Box>
+        {residente?.cargahorariacomplementar.map((elem) => (
+          <Box mb={2} key={elem.id}>
+            <Typography>#ID: {elem.id}</Typography>
+            <Typography>
+              Quantidade de horas: {elem.cargaHoraria} horas
+            </Typography>
+            <Typography>
+              Tipo CH coomplementar:{' '}
+              {elem.tipoCargaHorariaComplementar.descricao}
+            </Typography>
+            <Typography>Tipo da CH: {elem.tipoCargaHoraria}</Typography>
+          </Box>
+        ))}
         <Typography>
-          {JSON.stringify(residente?.cargahorariacomplementar, null, 2)}
+          Total de horas de CH complementares: {totalCHComplementates} horas
         </Typography>
       </DialogContent>
       <DialogActions>
