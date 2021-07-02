21d7a1fb3a36dd74deeffd155d2f342fca3c9fb2
ericsonmoreira <moreira.ericson@gmail.com>
2021-07-02 11:46:09 -0300

    adjusts in TurmaCardInfo component

--- src/components/TurmaCardInfo/index.tsx
+++ src/components/TurmaCardInfo/index.tsx
@@ -1,15 +1,16 @@
-import { Button, Card, CardContent, Typography } from '@material-ui/core';
+import {
+  Box,
+  Button,
+  Card,
+  CardContent,
+  Grid,
+  Typography,
+} from '@material-ui/core';
 import ArrowForwardIcon from '@material-ui/icons/ArrowForward';
 import { useHistory } from 'react-router';
 import NAMES from 'src/routes/names';
 import LabelAndInfo from '../LabelAndInfo';
-import {
-  CardActions,
-  ChipFim,
-  ChipInicio,
-  DatesWrapper,
-  InfoWrapper,
-} from './styles';
+import { CardActions, ChipFim, ChipInicio } from './styles';
 
 export interface TurmaCardInfoData {
   id: number;
@@ -43,22 +44,33 @@ const TurmaCardInfo: React.FC<TurmaCardInfoProps> = (props) => {
         <Typography variant="h6" gutterBottom>
           #{data.id} - {data.descricao}
         </Typography>
-        <InfoWrapper>
-          <LabelAndInfo
-            label="Período de duração"
-            info={`${data.quantidadeperiodo} anos`}
-            color="textSecondary"
-          />
-          <LabelAndInfo
-            label="Componente"
-            info={String(data.componente)}
-            color="textSecondary"
-          />
-        </InfoWrapper>
-        <DatesWrapper>
-          <ChipInicio variant="outlined" label={`Início: ${data.dataInicio}`} />
-          <ChipFim variant="outlined" label={`Fim: ${data.dataFim}`} />
-        </DatesWrapper>
+        <Grid container spacing={1} component={Box} mb={2}>
+          <Grid item>
+            <LabelAndInfo
+              label="Período de duração"
+              info={`${data.quantidadeperiodo} anos`}
+              color="textSecondary"
+            />
+          </Grid>
+          <Grid item>
+            <LabelAndInfo
+              label="Componente"
+              info={`${data.componente}`}
+              color="textSecondary"
+            />
+          </Grid>
+        </Grid>
+        <Grid container spacing={1}>
+          <Grid item>
+            <ChipInicio
+              variant="outlined"
+              label={`Início: ${data.dataInicio}`}
+            />
+          </Grid>
+          <Grid item>
+            <ChipFim variant="outlined" label={`Fim: ${data.dataFim}`} />
+          </Grid>
+        </Grid>
       </CardContent>
       <CardActions>
         <Button
