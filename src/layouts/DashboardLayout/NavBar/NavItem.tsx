692c4954169348209a850694e0153cb60ba1b8e4
Ericson Moreira <ericson.moreira@esp.ce.gov.br>
2023-04-28 09:08:15 -0300

    🐛 fix in NavItem

--- src/layouts/DashboardLayout/NavBar/NavItem.tsx
+++ src/layouts/DashboardLayout/NavBar/NavItem.tsx
@@ -1,33 +1,34 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { NavLink as RouterLink } from 'react-router-dom';
-import clsx from 'clsx';
 import { Button, ListItem, makeStyles } from '@material-ui/core';
+import { styled } from '@material-ui/core/styles';
+import { NavLink as RouterLink } from 'react-router-dom';
+
+const CustonListItem = styled(ListItem)({
+  display: 'flex',
+  paddingTop: 0,
+  paddingBottom: 0,
+});
 
 const useStyles = makeStyles((theme) => ({
-  item: {
-    display: 'flex',
-    paddingTop: 0,
-    paddingBottom: 0,
-  },
-  button: {
-    color: theme.palette.text.secondary,
-    fontWeight: theme.typography.fontWeightMedium,
-    justifyContent: 'flex-start',
-    letterSpacing: 0,
-    padding: '10px 8px',
-    textTransform: 'none',
-    width: '100%',
-  },
   icon: {
     marginRight: theme.spacing(1),
   },
   title: {
     marginRight: 'auto',
   },
+  button: {
+    color: theme.palette.text.secondary,
+    fontWeight: 500,
+    justifyContent: 'flex-start',
+    letterSpacing: 0,
+    padding: '10px 8px',
+    textTransform: 'none',
+    width: '100%',
+  },
   active: {
     color: theme.palette.primary.main,
     '& $title': {
-      fontWeight: theme.typography.fontWeightMedium,
+      fontWeight: 700,
     },
     '& $icon': {
       color: theme.palette.primary.main,
@@ -39,21 +40,12 @@ const NavItem = ({ className, href, icon: Icon, title, ...rest }: any) => {
   const classes = useStyles();
 
   return (
-    <ListItem
-      className={clsx(classes.item, className)}
-      disableGutters
-      {...rest}
-    >
-      <Button
-        activeClassName={classes.active}
-        className={classes.button}
-        component={RouterLink}
-        to={href}
-      >
+    <CustonListItem className={className} disableGutters {...rest}>
+      <Button activeClassName={classes.active} component={RouterLink} to={href}>
         {Icon && <Icon className={classes.icon} size="20" />}
         <span className={classes.title}>{title}</span>
       </Button>
-    </ListItem>
+    </CustonListItem>
   );
 };
 
